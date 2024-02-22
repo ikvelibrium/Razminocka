383f3b430208641ef9db789203cf997b575c3d3f
ikvelibrium <stalker.alex.perel@mail.ru>
2024-02-22 18:10:55 +0300

    rsy

--- Razminochka/Assets/_Source/Player/PlayerController.cs
+++ Razminochka/Assets/_Source/Player/PlayerController.cs
@@ -9,10 +9,19 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _hSpeed;
     [SerializeField] private float _vSpeed;
     [SerializeField] private Rigidbody2D rb;
-
+    Dictionary<string, object> aaa = new Dictionary<string, object>();
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        aaa.Add("a", 1);
+        aaa.Add("b", 2);
+        aaa.Add("c", 3);
+
+        AppMetrica.Instance.ReportEvent("1");
+        AppMetrica.Instance.ReportEvent("2");
+        AppMetrica.Instance.ReportEvent("3");
+        AppMetrica.Instance.ReportEvent("4");
+        AppMetrica.Instance.ReportEvent("5");
     }
 
     public void MooveRight()
@@ -28,6 +37,7 @@ public class PlayerController : MonoBehaviour
     {
         if ((( 1 << collision.gameObject.layer) & _deathLayer) > 0)
         {
+            AppMetrica.Instance.ReportEvent("LolYouDIed");
             SceneManager.LoadScene(0);
         }       
     }
--- Razminochka/Assets/_Source/StartGame.cs
+++ Razminochka/Assets/_Source/StartGame.cs
@@ -6,7 +6,7 @@ public class StartGame : MonoBehaviour
 {
     private void Start()
     {
-        Time.timeScale = 0;
+        
     }
     private void Update()
     {
